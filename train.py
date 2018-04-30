3ed201ba1dfcc89305d81a0a5c371d33e1babff8
Victor Escorcia <escorciav@gmail.com>
2018-04-30 11:09:56 +0300

    [src] train loop that achieves good results
    Training loop to achieve results close to Lisa's result for RGB data. In
    a nutshell, use SGD and similar hyper-parameters.
    
    - Replace print by logging to get a sense of time

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import random
 from pathlib import Path
 
@@ -16,6 +17,9 @@ from utils import Multimeter
 RAW_PATH = Path('data/raw')
 MODALITY = ['rgb', 'flow']
 EVAL_BATCH_SIZE = 1
+
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',
+                    level=logging.DEBUG)
 
 parser = argparse.ArgumentParser(description='DiDeMo')
 
@@ -47,16 +51,24 @@ parser.add_argument('--model_name', type=str, default='test',
 parser.add_argument('--seed', type=int, default=1,
                             help='Initial Random Seed')
 
-parser.add_argument('--optimizer', type=str, default='adam',
-                            help='optimizer')
 parser.add_argument('--momentum', type=float, default=0.9,
                     help='Nesterov Momentum for SGD')
 
+# Added for debugging
+parser.add_argument('--rec-layers', type=int, default=1,
+                    help='LSTM layers')
+
 args = parser.parse_args()
+
+# Testing different learning rate
+LR = [1e-1, 1e-2, 1e-3, 1e-4]
+random.shuffle(LR)
+args.lr = LR[0]
+
 args.device = 'cpu'
 if args.gpu_id >= 0 and torch.cuda.is_available():
     args.device = torch.device(f'cuda:{args.gpu_id}')
-print(args)
+logging.info(args)
 
 rgb_feat_path = RAW_PATH / 'average_fc7.h5'
 flow_feat_path = RAW_PATH / 'average_global_flow.h5'
@@ -73,7 +85,7 @@ else:
 # np.random.seed(args.seed)
 # random.seed(args.seed)
 
-print('Pre-loading features... This may take a couple of minutes.')
+logging.info('Pre-loading features... This may take a couple of minutes.')
 train_dataset = Didemo(train_list_path, rgb_file=rgb_feat_path,
                        flow_file=flow_feat_path)
                        # loc=True, max_words=50)
@@ -81,7 +93,7 @@ val_dataset = Didemo(val_list_path, rgb_file=rgb_feat_path,
                      flow_file=flow_feat_path, test=True)
 
 # Setup data loaders
-print('Setting-up loaders')
+logging.info('Setting-up loaders')
 train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size,
                               shuffle=True, num_workers=args.n_cpu,
                               collate_fn=train_dataset.collate_data)
@@ -90,37 +102,33 @@ val_dataloader = DataLoader(val_dataset, batch_size=EVAL_BATCH_SIZE,
                             collate_fn=val_dataset.collate_test_data)
 
 # Model
-print('Setting-up model')
+logging.info('Setting-up model')
 feat_0 = train_dataset[0]
 text_dim = feat_0[0].shape[1]
 video_modality_dim = feat_0[2].shape[0]
 max_length = feat_0[0].shape[0]
 net = MCN(visual_size=video_modality_dim, lang_size=text_dim,
-          max_length=max_length)
+          max_length=max_length, rec_layers=args.rec_layers)
 net.train()
 if args.gpu_id >= 0:
-    print('Transferring model to GPU')
+    logging.info('Transferring model to GPU')
     net.to(args.device)
 
 # Criterion
-print('Setting-up criterion')
+logging.info('Setting-up criterion')
 ranking_loss = IntraInterRanking(margin=args.margin)
 if args.gpu_id >= 0:
-    print('Transferring criterion to GPU')
+    logging.info('Transferring criterion to GPU')
     ranking_loss.to(args.device)
 
 # Optimizer
-print('Setting-up optimizer')
-if args.optimizer == 'adam':
-    optimizer = optim.Adam(net.parameters(), lr=args.lr)
-elif args.optimizer == 'sgd':
-    optimizer = optim.SGD(net.parameters(), lr=args.lr,
-                          momentum=args.momentum)
+logging.info('Setting-up optimizer')
+optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=args.momentum)
 
-print('Starting optimization...')
+logging.info('Starting optimization...')
 for epoch in range(args.epochs):
     running_loss = 0.0
-    print(f'Epoch: {epoch + 1}')
+    logging.info(f'Epoch: {epoch + 1}')
 
     # TODO: wrap it into a function
     for it, minibatch in enumerate(train_dataloader):
@@ -136,13 +144,13 @@ for epoch in range(args.epochs):
 
         running_loss += loss.item()
         if (it + 1) % args.n_display == 0:
-            print(f'Epoch: {epoch + 1}, '
+            logging.info(f'Epoch: {epoch + 1}, '
                   f'status: {it / len(train_dataloader):.2f}, '
                   f'loss: {running_loss / args.n_display:.4f}')
             running_loss = 0.0
 
     # TODO: wrap it into a function
-    print(f'* Evaluation')
+    logging.info(f'* Evaluation')
     meters = Multimeter(keys=['iou', 'r@1', 'r@5'])
     net.eval()
     for it, minibatch in enumerate(val_dataloader):
@@ -160,7 +168,7 @@ for epoch in range(args.epochs):
         gt = val_dataset.metadata[it]['times']
         performance = video_evaluation(gt, predictions)
         meters.update(performance)
-    print(f'{meters.report()}')
+    logging.info(f'{meters.report()}')
     net.train()
 
     # TODO: wrap it into a function

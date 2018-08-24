a2c65af161799eb39cf7c49ad4520f5e008c6c00
Victor Escorcia <escorciav@gmail.com>
2018-08-24 15:27:05 -0700

    [scripts] Add missing files for testing
    A couple of files and appropriate setup of them were missing. Fixing
    that issue and getting ready to v0.1

--- scripts/setup_data.sh
+++ scripts/setup_data.sh
@@ -16,8 +16,13 @@ yfcc_resnet152_dir=data/interim/yfcc100m/resnet152
 [ ! -d $yfcc_resnet152_dir ] && mkdir $yfcc_resnet152_dir
 [ ! -f $yfcc_resnet152_dir/320x240_001.h5 ] && ln -s $escorcia_folder/$yfcc_resnet152_dir/320x240_001.h5 $yfcc_resnet152_dir/
 
+# didemo-yfcc100m
+filename=data/interim/didemo_yfcc100m/train_data.json
+[ ! -d $(basename $filename) ] && mkdir -p $(basename $filename)
+[ ! -f $filename ] &&  ln -s $escorcia_folder/$i $i
+
 # data for corpus retrieval evaluation
-didemo_mcn_dir=data/interim/mcn/
+didemo_mcn_dir=data/interim/mcn
 [ ! -d $didemo_mcn_dir ] && mkdir $didemo_mcn_dir
 for i in corpus_val_rgb.hdf5 queries_val_rgb.hdf5; do
   [ ! -f $didemo_mcn_dir/$i ] && ln -s $escorcia_folder/$didemo_mcn_dir/$i $didemo_mcn_dir/
@@ -42,7 +47,7 @@ filename=data/processed/test/hsmcn_10_checkpoint.pth.tar
 gif_dir=dashboards/static/gif
 [ ! -d $gif_dir ] && ln -s /mnt/ilcompf9d1/user/escorcia/gif-didemo-val $gif_dir
 # moment-retrieval demo
-for i in data/interim/mcn/corpus_val_rgb.hdf5 data/interim/mcn/corpus_val_flow.hdf5 data/interim/mcn/rgb-weights.hdf5 data/interim/mcn/flow-weights.hdf5; do
+for i in data/interim/mcn/corpus_val_flow.hdf5 data/interim/mcn/rgb-weights.hdf5 data/interim/mcn/flow-weights.hdf5; do
   [ ! -f $filename ] && ln -s $escorcia_folder/$i $i
 done
 # sentence-retrieval
@@ -50,4 +55,5 @@ filename=data/processed/test/smcn_12_5_sr_rest.json
 [ ! -f $filename ] && ln -s $escorcia_folder/$filename $filename
 # moment explorer
 filename=data/interim/mcn_retrieval_results/rest_val_intra+inter_rgb+flow.json
+[ ! -d $(basename $filename) ] && mkdir -p $(basename $filename)
 [ ! -f $filename ] && ln -s $escorcia_folder/$i $i

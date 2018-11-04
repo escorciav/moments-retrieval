00a2cc084b0719d97f425c5e582b3ccd60b3ecfa
Victor Escorcia <escorciav@gmail.com>
2018-11-04 17:49:53 +0300

    [src] Update moment frequency prior program
    Update evaluation and model to estimate the moment frequency prior in a
    given dataset.
    
    - Update evaluation function and all the surroundings.
    
    - Replace the hacky discrete approach for a space discretization
      technique similar to KDE. In this way, the frequency prior is
      independent of the search space 😉 and considers the video length.

--- moment_freq_prior.py
+++ moment_freq_prior.py
@@ -19,26 +19,24 @@ from scipy.stats import gaussian_kde
 import dataset_untrimmed
 import proposals
 from evaluation import single_moment_retrieval
-from np_segments_ops import iou as segment_iou
 from np_segments_ops import non_maxima_suppresion
 from utils import setup_logging
 from utils import Multimeter
 
-# TODO(tier-2;release): mirror this constants from init file
-TOPK_IOU_POINTS = tuple(product((1, 5), (0.5, 0.7)))
-METRICS = [f'r@{k},{iou}' for k, iou in TOPK_IOU_POINTS]
+# TODO(tier-2;release): mirror this constants from an init file
+TOPK, IOU_THRESHOLDS = (1, 5), (0.5, 0.7)
+METRICS = [f'r@{k},{iou}' for iou, k in product(IOU_THRESHOLDS, TOPK)]
 
 parser = argparse.ArgumentParser(description='Frequency prior')
 parser.add_argument('--train-list', type=Path, default='non-existent',
                     help='JSON-file with training instances')
 parser.add_argument('--test-list', type=Path, default='non-existent',
                     help='JSON-file with training instances')
-# Type pf freq prior approximation
+# Freq prior parameters
 parser.add_argument('--kde', action='store_true',
                     help='Perform continous analysis')
-# Features
-parser.add_argument('--feat', default='tef',
-                    help='Record the type of feature used (modality)')
+parser.add_argument('--bins', type=int, default=10,
+                    help='Number of bins for discretization')
 # Hyper-parameters to explore search space (inference)
 parser.add_argument('--min-length', type=float, default=3,
                     help='Minimum length of slidding windows (seconds)')
@@ -47,7 +45,7 @@ parser.add_argument('--num-scales', type=int, default=8,
                          'window')
 parser.add_argument('--stride', type=float, default=3,
                     help='stride of the slidding window (seconds)')
-parser.add_argument('--nms-threshold', type=float, default=0.5)
+parser.add_argument('--nms-threshold', type=float, default=0.6)
 # Logging
 parser.add_argument('--logfile', type=Path, default='', help='Logging file')
 
@@ -55,42 +53,43 @@ args = parser.parse_args()
 
 
 def main(args):
+    "Glue all the pieces to estimate the moment frequency prior"
+    global TOPK
+    TOPK = torch.tensor(TOPK)
     setup_logging(args)
     logging.info('Moment frequency prior')
     logging.info(args)
     logging.info('Setting-up datasets')
     train_dataset, test_dataset = setup_dataset(args)
-    logging.info('Estimating prior')
-    moment_freq_prior = DiscreteFrequencyPrior()
     if args.kde:
         moment_freq_prior = KDEFrequencyPrior()
-    # "training"
+    else:
+        moment_freq_prior = DiscretizedFrequencyPrior(args.bins)
+
+    logging.info('Estimating prior')
     for i, data in enumerate(train_dataset):
-        gt_segments, pred_segments = data[-2:]
+        gt_moments = data[-2]
         duration_i = video_duration(train_dataset, i)
-        moment_freq_prior.update(gt_segments, pred_segments, duration_i)
+        moment_freq_prior.update(gt_moments, duration_i)
     logging.info('Model fitting')
     moment_freq_prior.fit()
 
-    # testing
     logging.info(f'* Evaluation')
-    if not args.kde:
-        pred_segments = torch.from_numpy(moment_freq_prior.predict())
     meters = Multimeter(keys=METRICS)
     for i, data in enumerate(test_dataset):
         duration_i = video_duration(train_dataset, i)
-        gt_segments = torch.from_numpy(data[-2])
-        if args.kde:
-            pred_segments_ = data[-1]
-            prob = moment_freq_prior.predict(pred_segments_, duration_i)
-            if args.nms_threshold < 1:
-                ind = non_maxima_suppresion(
-                    pred_segments_, prob, args.nms_threshold)
-            else:
-                ind = prob.argsort()[::-1]
-            pred_segments = torch.from_numpy(pred_segments_[ind, :])
+        gt_moments = torch.from_numpy(data[-2])
+        proposals_i = data[-1]
+        prob = moment_freq_prior.predict(proposals_i, duration_i)
+        if args.nms_threshold < 1:
+            ind = non_maxima_suppresion(
+                proposals_i, prob, args.nms_threshold)
+        else:
+            ind = prob.argsort()[::-1]
+        sorted_proposals = proposals_i[ind, :]
+        sorted_proposals = torch.from_numpy(sorted_proposals)
         hit_k_iou = single_moment_retrieval(
-            gt_segments, pred_segments, TOPK_IOU_POINTS)
+            gt_moments, sorted_proposals, topk=TOPK)
         meters.update([i.item() for i in hit_k_iou])
     logging.info(f'{meters.report()}')
 
@@ -105,77 +104,73 @@ def setup_dataset(args):
     "Setup dataset and loader"
     proposal_generator = proposals.SlidingWindowMSFS(
         args.min_length, args.num_scales, args.stride, unique=True)
+    lastname = 'kde' if args.kde else 'discrete'
+    cues = {f'mfp-{lastname}': None}
     subset_files = [('train', args.train_list), ('test', args.test_list)]
     datasets = []
     for i, (subset, filename) in enumerate(subset_files):
         datasets.append(
             dataset_untrimmed.UntrimmedMCN(
                 filename, proposals_interface=proposal_generator,
-                # we don't care about language only visual for lazy reasons
-                # similarly we don't care about eval or train mode
-                eval=True, debug=True, cues=None, no_visual=True, loc=True)
+                # we don't care about language only visual and there is not
+                # distintion btw eval or train mode
+                eval=True, debug=True, cues=cues, no_visual=True, loc=True)
         )
     return datasets
 
 
-class DiscreteFrequencyPrior():
-    "Compute the frequency prior of segments in dataset"
-
-    def __init__(self):
-        self.table = {}
-        self.num_segments = None
-        self.frequency = None
-        self.segments = None
-
-    def update(self, gt_segments, pred_segments, duration=None):
-        "Count how often a given segment appears"
-        # TODO: use a soft-criteria such as IOU over pred_segments
-        ind = np.unique(
-            segment_iou(pred_segments, gt_segments).argmax(axis=0))
-        gt_segments_mapped_to_pred = pred_segments[ind, :]
-        assert len(gt_segments_mapped_to_pred.shape) == 2
-        for segment in gt_segments_mapped_to_pred:
-            hashable_segment = tuple(segment.tolist())
-            if hashable_segment not in self.table:
-                self.table[hashable_segment] = 0
-            self.table[hashable_segment] += 1
-
-    def fit(self):
-        "Sort table"
-        assert len(self.table) > 0
-        self.num_segments = len(self.table)
-        segments = np.array(list(self.table.keys()))
-        frequency = np.array(list(self.table.values()))
-        ind = np.argsort(frequency)
-        self.frequency = frequency[ind]
-        self.segments = segments[ind, :].astype(np.float32)
-
-    def predict(self, pred_segments=None, duration=None):
-        return self.segments
-
-
-class KDEFrequencyPrior():
+class BaseFrequencyPrior():
     "Compute the frequency prior of segments in dataset"
 
     def __init__(self):
         self.table = []
-        self.num_segments = None
         self.model = None
 
-    def update(self, gt_segments, pred_segments=None, duration=None):
+    def update(self, gt_segments, duration):
         "Count how often a given segment appears"
-        assert duration is not None
         self.table.append(gt_segments / duration)
 
+
+class DiscretizedFrequencyPrior(BaseFrequencyPrior):
+    "Estimate frequency prior of segments in dataset via discretization"
+
+    def __init__(self, bins):
+        super(DiscretizedFrequencyPrior, self).__init__()
+        self.bins = bins
+        self._x_edges, self._y_edges = None, None
+
     def fit(self):
-        "Sort table"
+        "Make 2D histogram"
+        table_np = np.row_stack(self.table)
+        self.model, self._x_edges, self._y_edges = np.histogram2d(
+            table_np[:, 0], table_np[:, 1], bins=self.bins, normed=True)
+
+    def predict(self, pred_segments=None, duration=None):
+        "Return prob that a proposal belongs to the dataset"
+        assert self.model is not None
+        normalized_proposals = pred_segments / duration
+        ind_x = self._search(self._x_edges, normalized_proposals[:, 0])
+        ind_y = self._search(self._y_edges, normalized_proposals[:, 1])
+        return self.model[ind_x, ind_y]
+
+    def _search(self, x, y):
+        ind = np.searchsorted(x, y) - 1
+        return np.clip(ind, 0, len(x) - 2)
+
+
+class KDEFrequencyPrior(BaseFrequencyPrior):
+    "Estimate frequency prior of segments in dataset via KDE"
+
+    def fit(self):
+        "Fit PDF with KDE and default bandwidth selection rule"
         all_segments = np.row_stack(self.table).T
         self.model = gaussian_kde(all_segments)
 
     def predict(self, pred_segments=None, duration=None):
+        "Return prob that a proposal belongs to the dataset"
         assert self.model is not None
-        normalized_segments = (pred_segments / duration).T
-        return self.model.pdf(normalized_segments)
+        normalized_proposal = (pred_segments / duration).T
+        return self.model.pdf(normalized_proposal)
 
 
 if __name__ == '__main__':

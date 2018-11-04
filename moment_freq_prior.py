33fceec90bc96286a7c50335ad2fda3df40fa727
Victor Escorcia <escorciav@gmail.com>
2018-11-04 22:52:09 +0300

    [src] Update moment frequency prior
    Refactor program to add original DiDeMo metrics (if possible), support
    edges for bins, and update command line arguments by making some
    arguments mandatory and making exclusive parameters obvious.
    
    - Remove NMS from DiDeMo.
    
    - Use `digitize` instead of `searchsorted`.
    
    - notebook: as cross-check with original DiDeMo evaluation based on
      Lisa's email.

--- moment_freq_prior.py
+++ moment_freq_prior.py
@@ -18,7 +18,7 @@ from scipy.stats import gaussian_kde
 
 import dataset_untrimmed
 import proposals
-from evaluation import single_moment_retrieval
+from evaluation import single_moment_retrieval, didemo_evaluation
 from np_segments_ops import non_maxima_suppresion
 from utils import setup_logging
 from utils import Multimeter
@@ -26,18 +26,31 @@ from utils import Multimeter
 # TODO(tier-2;release): mirror this constants from an init file
 TOPK, IOU_THRESHOLDS = (1, 5), (0.5, 0.7)
 METRICS = [f'r@{k},{iou}' for iou, k in product(IOU_THRESHOLDS, TOPK)]
+METRICS_OLD = ['iou', 'r@1', 'r@5']
 
 parser = argparse.ArgumentParser(description='Frequency prior')
-parser.add_argument('--train-list', type=Path, default='non-existent',
+parser.add_argument('--train-list', type=Path, required=True,
                     help='JSON-file with training instances')
-parser.add_argument('--test-list', type=Path, default='non-existent',
+parser.add_argument('--test-list', type=Path, required=True,
                     help='JSON-file with training instances')
 # Freq prior parameters
-parser.add_argument('--kde', action='store_true',
-                    help='Perform continous analysis')
-parser.add_argument('--bins', type=int, default=10,
-                    help='Number of bins for discretization')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('--kde', action='store_true',
+                   help='Perform continous analysis')
+group.add_argument('--bins', type=int,
+                   help=('Number of bins for discretization. Please provide '
+                         'something for DiDeMo, but would be ignored in favor'
+                         'of the ICCV-2017 bin edges.')
+                  )
+group_edges = group.add_argument_group(description='Bins for discretization')
+group_edges.add_argument('--ts-edges', type=float, nargs='+',
+                         help='Bin edges for t-start')
+group_edges.add_argument('--te-edges', type=float, nargs='+',
+                         help='Bin edges for t-end')
 # Hyper-parameters to explore search space (inference)
+parser.add_argument('--proposal-interface', default='SlidingWindowMSFS',
+                    choices=proposals.PROPOSAL_SCHEMES,
+                    help='Type of proposals spanning search space')
 parser.add_argument('--min-length', type=float, default=3,
                     help='Minimum length of slidding windows (seconds)')
 parser.add_argument('--num-scales', type=int, default=8,
@@ -45,7 +58,11 @@ parser.add_argument('--num-scales', type=int, default=8,
                          'window')
 parser.add_argument('--stride', type=float, default=3,
                     help='stride of the slidding window (seconds)')
-parser.add_argument('--nms-threshold', type=float, default=0.6)
+parser.add_argument('--nms-threshold', type=float, default=0.6,
+                    help=('Threshold used to remove overlapped predictions'
+                          'We use 1.0 in DiDeMo for fair comparsion.'
+                          'Moreover the evaluation code also assumes that.')
+                    )
 # Logging
 parser.add_argument('--logfile', type=Path, default='', help='Logging file')
 
@@ -56,6 +73,7 @@ def main(args):
     "Glue all the pieces to estimate the moment frequency prior"
     global TOPK
     TOPK = torch.tensor(TOPK)
+    TOPK_ = TOPK.float()
     setup_logging(args)
     logging.info('Moment frequency prior')
     logging.info(args)
@@ -64,6 +82,12 @@ def main(args):
     if args.kde:
         moment_freq_prior = KDEFrequencyPrior()
     else:
+        if args.proposal_interface == 'DidemoICCV17SS':
+            logging.info('Ignoring bins argument')
+            args.ts_edges = np.arange(0, 31, 5.0) / 30
+            args.te_edges = np.arange(5, 36, 5.0) / 30
+        if args.ts_edges is not None:
+            args.bins = [args.ts_edges, args.te_edges]
         moment_freq_prior = DiscretizedFrequencyPrior(args.bins)
 
     logging.info('Estimating prior')
@@ -75,23 +99,38 @@ def main(args):
     moment_freq_prior.fit()
 
     logging.info(f'* Evaluation')
-    meters = Multimeter(keys=METRICS)
+    meters, meters_old = Multimeter(keys=METRICS), None
+    if args.proposal_interface == 'DidemoICCV17SS':
+        meters_old = Multimeter(keys=METRICS_OLD)
+        meters_old_ = Multimeter(keys=METRICS_OLD)
+        # Details are provided in help
+        args.nms_threshold = 1.0
     for i, data in enumerate(test_dataset):
         duration_i = video_duration(train_dataset, i)
         gt_moments = torch.from_numpy(data[-2])
         proposals_i = data[-1]
         prob = moment_freq_prior.predict(proposals_i, duration_i)
+
         if args.nms_threshold < 1:
             ind = non_maxima_suppresion(
                 proposals_i, prob, args.nms_threshold)
         else:
             ind = prob.argsort()[::-1]
+
         sorted_proposals = proposals_i[ind, :]
         sorted_proposals = torch.from_numpy(sorted_proposals)
         hit_k_iou = single_moment_retrieval(
             gt_moments, sorted_proposals, topk=TOPK)
+
         meters.update([i.item() for i in hit_k_iou])
+        if meters_old:
+            iou_r_at_ks = didemo_evaluation(
+                gt_moments, sorted_proposals, TOPK_)
+            meters_old.update([i.item() for i in iou_r_at_ks])
+
     logging.info(f'{meters.report()}')
+    if meters_old:
+        logging.info(f'{meters_old.report()}')
 
 
 def video_duration(dataset, moment_index):
@@ -102,7 +141,7 @@ def video_duration(dataset, moment_index):
 
 def setup_dataset(args):
     "Setup dataset and loader"
-    proposal_generator = proposals.SlidingWindowMSFS(
+    proposal_generator = proposals.__dict__[args.proposal_interface](
         args.min_length, args.num_scales, args.stride, unique=True)
     lastname = 'kde' if args.kde else 'discrete'
     cues = {f'mfp-{lastname}': None}
@@ -135,27 +174,24 @@ class DiscretizedFrequencyPrior(BaseFrequencyPrior):
     "Estimate frequency prior of segments in dataset via discretization"
 
     def __init__(self, bins):
+        "Edges have priority over bins"
         super(DiscretizedFrequencyPrior, self).__init__()
-        self.bins = bins
         self._x_edges, self._y_edges = None, None
+        self.bins = bins
 
     def fit(self):
         "Make 2D histogram"
         table_np = np.row_stack(self.table)
         self.model, self._x_edges, self._y_edges = np.histogram2d(
-            table_np[:, 0], table_np[:, 1], bins=self.bins, normed=True)
+            table_np[:, 0], table_np[:, 1], bins=self.bins)
 
     def predict(self, pred_segments=None, duration=None):
         "Return prob that a proposal belongs to the dataset"
         assert self.model is not None
         normalized_proposals = pred_segments / duration
-        ind_x = self._search(self._x_edges, normalized_proposals[:, 0])
-        ind_y = self._search(self._y_edges, normalized_proposals[:, 1])
+        ind_x = np.digitize(normalized_proposals[:, 0], self._x_edges, True)
+        ind_y = np.digitize(normalized_proposals[:, 1], self._y_edges, True)
         return self.model[ind_x, ind_y]
-
-    def _search(self, x, y):
-        ind = np.searchsorted(x, y) - 1
-        return np.clip(ind, 0, len(x) - 2)
 
 
 class KDEFrequencyPrior(BaseFrequencyPrior):

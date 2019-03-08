006aea1263b7d8ea48a8834a73985a37c919c794
Victor Escorcia <escorciav@gmail.com>
2019-03-08 19:26:05 +0300

    [src] Add 2nd stage moment retrieval from video corpus
    Program to perform and evaluate retrieval of moments from a video corpus
    re-using results from a previous retrieval stage. Update
    `corpus_retrieval_eval` to report if approximated search was triggered
    and statistics about it.

--- corpus_retrieval_2nd_eval.py
+++ corpus_retrieval_2nd_eval.py
@@ -9,40 +9,32 @@ import numpy as np
 import torch
 from tqdm import tqdm
 
-import corpus
 import dataset_untrimmed
 import model
 import proposals
+from corpus import LoopOverKVideos
 from evaluation import CorpusVideoMomentRetrievalEval
 from utils import setup_logging, get_git_revision_hash
 
-# TODO(tier-2;clean): remove this hard-coded approach
-# we not only use the same arch, but also the same hyper-prm
-UNIQUE_VARS = {key: [] for key in
-               ['arch', 'loc', 'context', 'proposal_interface']}
-
 parser = argparse.ArgumentParser(
-    description='Corpus Retrieval Evaluation',
+    description='Corpus Retrieval 2nd Stage Evaluation',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 # Data
 parser.add_argument('--test-list', type=Path, required=True,
                     help='JSON-file with corpus instances')
-parser.add_argument('--h5-path', type=Path, nargs='+',
+parser.add_argument('--h5-path', type=Path, default='non-existent',
                     help='HDF5-file with features')
-parser.add_argument('--tags', nargs='+',
-                    help='Tag for h5-file features')
 # Architecture
-parser.add_argument('--snapshot', type=Path, required=True, nargs='+',
-                    help='JSON files of model')
-parser.add_argument('--snapshot-tags', nargs='+',
-                    help='Pair model to a given h5-path')
+parser.add_argument('--snapshot', type=Path, required=True,
+                    help='JSON-file of model')
+parser.add_argument('--h5-1ststage', type=Path, required=True,
+                    help='HDF5-file of 1st stage results')
+parser.add_argument('--k-first', type=int, required=True,
+                    help='K first retrieved resuslts')
 # Evaluation parameters
 parser.add_argument('--topk', nargs='+', type=int,
-                    default=[1, 10, 100, 1000, 10000],
-                    help='top-k values to compute')
-# Extra
-parser.add_argument('--greedy', type=int, default=0,
-                    help='Top-k seed clips for greedy search over clips')
+                    default=[1, 10, 100],
+                    help='top-k values to compute in ascending order.')
 # Dump results and logs
 parser.add_argument('--dump', action='store_true',
                     help='Save log in text file and json')
@@ -52,8 +44,6 @@ parser.add_argument('--n-display', type=float, default=0.2,
                     help='logging rate during epoch')
 parser.add_argument('--disable-tqdm', action='store_true',
                     help='Disable progress-bar')
-parser.add_argument('--dump-per-instance-results', action='store_true',
-                    help='HDF5 with results')
 # Debug
 parser.add_argument('--debug', action='store_true',
                     help=('yield incorrect results! to verify things are'
@@ -66,42 +56,33 @@ def main(args):
     if args.dump:
         args.disable_tqdm = True
         if len(args.logfile.name) == 0:
-            basename_fusion = [str(i.with_suffix('').with_name(i.stem))
-                               for i in args.snapshot]
-            args.logfile = Path('-'.join(basename_fusion) + '_corpus-eval')
+            basename = args.snapshot.with_suffix('')
+            args.logfile = basename.with_name(
+                args.snapshot.stem + '_corpus-2nd-eval')
         if args.logfile.exists():
             raise ValueError(
                 f'{args.logfile} already exists. Please provide a logfile or'
                 'backup existing results.')
     setup_logging(args)
 
-    logging.info('Corpus Retrieval Evaluation for CAL/MCN')
-    logging.info(f'Git revision hash: {get_git_revision_hash()}')
+    logging.info('Corpus Retrieval Evaluation for 2nd Stage')
     load_hyperparameters(args)
     logging.info(args)
 
     engine_prm = {}
     if args.arch == 'MCN':
         args.dataset = 'UntrimmedMCN'
-        args.engine = 'MomentRetrievalFromProposalsTable'
     elif args.arch == 'SMCN':
         args.dataset = 'UntrimmedSMCN'
-        args.engine = 'MomentRetrievalFromClipBasedProposalsTable'
-        if args.greedy > 0:
-            args.engine = 'GreedyMomentRetrievalFromClipBasedProposalsTable'
-            engine_prm['topk'] = args.greedy
     else:
         ValueError('Unknown/unsupported architecture')
-    if args.greedy > 0 and args.arch != 'SMCN':
-        logging.warning('Ignore greedy search. Unsupported model')
 
     logging.info('Loading dataset')
-    dataset_novisual = True
-    dataset_cues = {feat: None for feat in args.tags}
-    if len(args.h5_path) > 0:
-        for i, key in enumerate(args.tags):
-            dataset_cues[key] = {'file': args.h5_path[i]}
+    if args.h5_path.exists():
         dataset_novisual = False
+        dataset_cues = {args.feat: {'file': args.h5_path}}
+    else:
+        raise NotImplementedError('WIP')
     proposals_interface = proposals.__dict__[args.proposal_interface]
     proposals_prm = dict(
         length=args.min_length, stride=args.stride,
@@ -118,27 +99,24 @@ def main(args):
         dataset.set_padding(False)
 
     logging.info('Setting up models')
-    models_dict = {}
-    for i, key in enumerate(args.snapshot_tags):
-        arch_setup = dict(
-            visual_size=dataset.visual_size[key],
-            lang_size=dataset.language_size,
-            max_length=dataset.max_words,
-            embedding_size=args.embedding_size,
-            visual_hidden=args.visual_hidden,
-            lang_hidden=args.lang_hidden,
-            visual_layers=args.visual_layers,
-        )
-        models_dict[key] = model.__dict__[args.arch](**arch_setup)
-        filename = args.snapshot[i].with_suffix('.pth.tar')
-        snapshot_ = torch.load(
-            filename, map_location=lambda storage, loc: storage)
-        models_dict[key].load_state_dict(snapshot_['state_dict'])
-        models_dict[key].eval()
+    arch_setup = dict(
+        visual_size=dataset.visual_size[args.feat],
+        lang_size=dataset.language_size,
+        max_length=dataset.max_words,
+        embedding_size=args.embedding_size,
+        visual_hidden=args.visual_hidden,
+        lang_hidden=args.lang_hidden,
+        visual_layers=args.visual_layers,
+    )
+    net = model.__dict__[args.arch](**arch_setup)
+    filename = args.snapshot.with_suffix('.pth.tar')
+    snapshot_ = torch.load(
+        filename, map_location=lambda storage, loc: storage)
+    net.load_state_dict(snapshot_['state_dict'])
+    net.eval()
 
-    logging.info('Creating database alas indexing corpus')
-    engine = corpus.__dict__[args.engine](dataset, models_dict, **engine_prm)
-    engine.indexing()
+    engine = LoopOverKVideos(
+        dataset, net, args.h5_1ststage, topk=args.k_first)
 
     logging.info('Launch evaluation...')
     # log-scale up to the end of the database
@@ -149,18 +127,10 @@ def main(args):
     num_instances_retrieved = []
     judge = CorpusVideoMomentRetrievalEval(topk=args.topk)
     args.n_display = max(int(args.n_display * len(dataset.metadata)), 1)
-    vid_indices_per_instance, prop_ind_per_instance = [], []
     for it, query_metadata in tqdm(enumerate(dataset.metadata),
                                    disable=args.disable_tqdm):
-        result_per_query = engine.query(
-            query_metadata['language_input'],
-            return_indices=args.dump_per_instance_results)
-        if args.dump_per_instance_results:
-            vid_indices, segments, proposals_ind = result_per_query
-            vid_indices_per_instance.append(vid_indices)
-            prop_ind_per_instance.append(proposals_ind)
-        else:
-            vid_indices, segments = result_per_query
+        vid_indices, segments = engine.query(
+            query_metadata['language_input'], description_ind=it)
         judge.add_single_predicted_moment_info(
             query_metadata, vid_indices, segments, max_rank=engine.num_moments)
         num_instances_retrieved.append(len(vid_indices))
@@ -187,57 +157,30 @@ def main(args):
         with open(filename, 'x') as fid:
             for key, value in result.items():
                 result[key] = float(value)
-            result['snapshot'] = [str(i) for i in args.snapshot]
+            result['snapshot'] = str(args.snapshot)
             result['corpus'] = str(args.test_list)
+            result['1ststage'] = str(args.h5_1ststage)
             result['topk'] = args.topk
             result['iou_threshold'] = judge.iou_thresholds
-            result['greedy'] = args.greedy
+            result['k_first'] = args.k_first
             result['median_proposals_retrieved'] = retrieved_proposals_median
             result['min_proposals_retrieved'] = retrieved_proposals_min
             result['date'] = datetime.now().isoformat()
             result['git_hash'] = get_git_revision_hash()
             json.dump(result, fid, indent=1)
 
-    if args.dump_per_instance_results:
-        filename = args.logfile.with_suffix('.h5')
-        with h5py.File(filename, 'x') as fid:
-            fid.create_dataset(
-                name='proposals', data=engine.proposals, chunks=True)
-            fid.create_dataset(
-                name='vid_indices', chunks=True,
-                data=torch.stack(vid_indices_per_instance))
-            fid.create_dataset(
-                name='proposals_ind', chunks=True,
-                data=torch.stack(prop_ind_per_instance))
-
 
 def load_hyperparameters(args):
     "Update args with model hyperparameters"
-    if len(args.tags) == 0:
-        single_model_and_cues = len(args.snapshot) == len(args.h5_path)
-        assert single_model_and_cues
-        # take tag of first model
-        logging.info('Set JSON files with hyper-parameters')
-        with open(args.snapshot[0], 'r') as fid:
-            hyper_prm = json.load(fid)
-            args.tags = [hyper_prm['feat']]
-            args.snapshot_tags = [args.tags[0]]
-
     logging.info('Parsing JSON files with hyper-parameters')
-    args.tags = dict.fromkeys(args.tags)
-    assert len(args.h5_path) == len(args.tags)
-    for i, filename in enumerate(args.snapshot):
-        with open(filename, 'r') as fid:
-            hyper_prm = json.load(fid)
-            assert args.snapshot_tags[i] in args.tags
-            for key, value in hyper_prm.items():
-                if not hasattr(args, key):
-                    setattr(args, key, value)
-                if key in UNIQUE_VARS:
-                    UNIQUE_VARS[key].append(value)
+    with open(args.snapshot, 'r') as fid:
+        hyper_prm = json.load(fid)
 
-    for value in UNIQUE_VARS.values():
-        assert len(np.unique(value)) == 1
+    for key, value in hyper_prm.items():
+        if not hasattr(args, key):
+            setattr(args, key, value)
+        else:
+            logging.debug(f'Ignored hyperparam: {key}')
 
 
 if __name__ == '__main__':
